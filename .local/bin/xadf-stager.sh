238c14203fb9f4fabf7bf7052951e1af0e9d2f30
Hendrik Lie <hendriklie72@gmail.com>
2023-02-20 21:09:49 +0800

    adds a helper script to rapidly stage branches

---
+++ .local/bin/xadf-stager.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# stager <source> <target>
+gitstager(){
+test -z "$1" && local source="trunk"
+test -z "$2" && local target="master"
+
+if [ ! -f ".git" ]
+then
+  echo "Not in a git repo!" >&2
+  exit 1
+else
+  git checkout $target && \
+  git pull && \
+  git merge $source && \
+  git push
+fi
+}
+
+git checkout development
+
+gitstager development trunk
+gitstager trunk master
+gitstager trunk termux
+
+git checkout development
